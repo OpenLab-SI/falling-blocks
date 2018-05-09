06abea4275fbb9abd5f1e9d0eafcf9ab3156d861
Metod Medja <metod@medja.net>
2018-05-09 19:02:00 +0200

    Dodan razred Square za kvadrate

--- core/src/si/openlab/falling_blocks/FallingBlocks.java
+++ core/src/si/openlab/falling_blocks/FallingBlocks.java
@@ -2,6 +2,7 @@ package si.openlab.falling_blocks;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,14 +11,19 @@ public class FallingBlocks extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
 
-    boolean firstClicked;
-    boolean secondClicked;
-    boolean thirdClicked;
+    Square firstSquare;
+    Square secondSquare;
+    Square thirdSquare;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         img = new Texture("square.png");
+
+        // vsak kvadrat ima podatke, kje je, katere barve je in ce je kliknjen
+        firstSquare = new Square(0, 0, img, Color.RED);
+        secondSquare = new Square(200, 150, img, Color.GREEN);
+        thirdSquare = new Square(400, 300, img, Color.BLUE);
     }
 
     @Override
@@ -26,40 +32,22 @@ public class FallingBlocks extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // preveri ce je kliknjen kateri izmed kvadratov
         if (Gdx.input.isButtonPressed(0)) {
             int x = Gdx.input.getX();
             int y = Gdx.graphics.getHeight() - Gdx.input.getY();
 
-            if (x >= 0 && x <= 0 + 100 && y >= 0 && y <= 0 + 100) {
-                firstClicked = true;
-            }
-
-            if (x >= 200 && x <= 200 + 100 && y >= 150 && y <= 150 + 100) {
-                secondClicked = true;
-            }
-
-            if (x >= 400 && x <= 400 + 100 && y >= 300 && y <= 300 + 100) {
-                thirdClicked = true;
-            }
+            firstSquare.checkClicked(x, y);
+            secondSquare.checkClicked(x, y);
+            thirdSquare.checkClicked(x, y);
         }
 
         // izrisi igro
         batch.begin();
 
-        if (!firstClicked) {
-            batch.setColor(1, 0, 0, 1);
-            batch.draw(img, 0, 0, 100, 100);
-        }
-
-        if (!secondClicked) {
-            batch.setColor(0, 1, 0, 1);
-            batch.draw(img, 200, 150, 100, 100);
-        }
-
-        if (!thirdClicked) {
-            batch.setColor(0, 0, 1, 1);
-            batch.draw(img, 400, 300, 100, 100);
-        }
+        firstSquare.draw(batch);
+        secondSquare.draw(batch);
+        thirdSquare.draw(batch);
 
         batch.end();
     }
