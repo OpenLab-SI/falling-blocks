4c3818b3a713862d4e7dea2eea2d49f81a290e1f
Metod Medja <metod@medja.net>
2018-05-09 17:20:19 +0200

    Izdelava libGDX projekta

---
+++ android/src/si/openlab/falling_blocks/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package si.openlab.falling_blocks;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import si.openlab.falling_blocks.FallingBlocks;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FallingBlocks(), config);
+	}
+}
---
+++ desktop/src/si/openlab/falling_blocks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package si.openlab.falling_blocks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import si.openlab.falling_blocks.FallingBlocks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FallingBlocks(), config);
+	}
+}
