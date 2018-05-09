7e6844edd65eca2cf479ba79f4380402d35227e7
Metod Medja <metod@medja.net>
2018-05-09 23:30:06 +0200

    Nastavitev velikosti okna

--- desktop/src/si/openlab/falling_blocks/desktop/DesktopLauncher.java
+++ desktop/src/si/openlab/falling_blocks/desktop/DesktopLauncher.java
@@ -5,8 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import si.openlab.falling_blocks.FallingBlocks;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FallingBlocks(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = "Falling blocks";
+        config.fullscreen = false;
+        config.resizable = true;
+        config.width = 720;
+        config.height = 480;
+        config.useHDPI = false;
+
+        new LwjglApplication(new FallingBlocks(), config);
+    }
 }
