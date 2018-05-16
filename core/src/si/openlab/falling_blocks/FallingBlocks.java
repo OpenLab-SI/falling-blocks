3b60b0879c0e5c8b63d1abebb32b1ba9577b7cc1
Metod Medja <metod@medja.net>
2018-05-16 17:26:38 +0200

    Odstranjen seznam kvadratov

--- core/src/si/openlab/falling_blocks/FallingBlocks.java
+++ core/src/si/openlab/falling_blocks/FallingBlocks.java
@@ -2,31 +2,22 @@ package si.openlab.falling_blocks;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class FallingBlocks extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
 
-    // seznam vseh kvadratov na zaslonu
-    // tega je nujno treba narediti (tako kot kvadrate)
-    List<Square> squares = new ArrayList<Square>();
+    boolean firstClicked;
+    boolean secondClicked;
+    boolean thirdClicked;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         img = new Texture("square.png");
-
-        // vsak kvadrat ima podatke, kje je, katere barve je in ce je kliknjen
-        squares.add(new Square(0, 0, img, Color.RED));
-        squares.add(new Square(200, 150, img, Color.GREEN));
-        squares.add(new Square(400, 300, img, Color.BLUE));
     }
 
     @Override
@@ -35,24 +26,39 @@ public class FallingBlocks extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // preveri ce je kliknjen kateri izmed kvadratov
         if (Gdx.input.isButtonPressed(0)) {
             int x = Gdx.input.getX();
             int y = Gdx.graphics.getHeight() - Gdx.input.getY();
 
-            // v zanki stejemo od 0 do stevila kvadratov (od 0 do 3)
-            // in preverimo ce je katerikoli kliknjen
-            for (int i = 0; i < squares.size(); i++) {
-                squares.get(i).checkClicked(x, y);
+            if (x >= 0 && x <= 0 + 100 && y >= 0 && y <= 0 + 100) {
+                firstClicked = true;
+            }
+
+            if (x >= 200 && x <= 200 + 100 && y >= 150 && y <= 150 + 100) {
+                secondClicked = true;
+            }
+
+            if (x >= 400 && x <= 400 + 100 && y >= 300 && y <= 300 + 100) {
+                thirdClicked = true;
             }
         }
 
         // izrisi igro
         batch.begin();
 
-        // izrisemo vse kvadrate s trenutnim batch-om
-        for (int i = 0; i < squares.size(); i++) {
-            squares.get(i).draw(batch);
+        if (!firstClicked) {
+            batch.setColor(1, 0, 0, 1);
+            batch.draw(img, 0, 0, 100, 100);
+        }
+
+        if (!secondClicked) {
+            batch.setColor(0, 1, 0, 1);
+            batch.draw(img, 200, 150, 100, 100);
+        }
+
+        if (!thirdClicked) {
+            batch.setColor(0, 0, 1, 1);
+            batch.draw(img, 400, 300, 100, 100);
         }
 
         batch.end();
