df8f23b90940cdce0d269b82926a8a11262f6493
Metod Medja <metod@medja.net>
2018-05-23 17:29:50 +0200

    Odstranjeno rocno izrisovanje kvadratov

--- core/src/si/openlab/falling_blocks/FallingBlocks.java
+++ core/src/si/openlab/falling_blocks/FallingBlocks.java
@@ -10,17 +10,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class FallingBlocks extends ApplicationAdapter {
-    SpriteBatch batch;
-
-    Texture img;
     AssetManager assets;
-
     OrthographicCamera camera;
     FitViewport viewport;
-
-    boolean firstClicked;
-    boolean secondClicked;
-    boolean thirdClicked;
 
     @Override
     public void create() {
@@ -37,11 +29,6 @@ public class FallingBlocks extends ApplicationAdapter {
         viewport = new FitViewport(720, 480, camera);
         // centriraj igro
         viewport.apply(true);
-
-        batch = new SpriteBatch();
-
-        // slika kvadrata pride iz vzdrzevalca virov
-        img = assets.get("square.png");
     }
 
     @Override
@@ -50,46 +37,8 @@ public class FallingBlocks extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (Gdx.input.isButtonPressed(0)) {
-            int x = Gdx.input.getX();
-            int y = Gdx.graphics.getHeight() - Gdx.input.getY();
-
-            if (x >= 0 && x <= 0 + 100 && y >= 0 && y <= 0 + 100) {
-                firstClicked = true;
-            }
-
-            if (x >= 200 && x <= 200 + 100 && y >= 150 && y <= 150 + 100) {
-                secondClicked = true;
-            }
-
-            if (x >= 400 && x <= 400 + 100 && y >= 300 && y <= 300 + 100) {
-                thirdClicked = true;
-            }
-        }
-
         // uporabi kamero
         camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
-        // izrisi igro
-        batch.begin();
-
-        if (!firstClicked) {
-            batch.setColor(1, 0, 0, 1);
-            batch.draw(img, 0, 0, 100, 100);
-        }
-
-        if (!secondClicked) {
-            batch.setColor(0, 1, 0, 1);
-            batch.draw(img, 200, 150, 100, 100);
-        }
-
-        if (!thirdClicked) {
-            batch.setColor(0, 0, 1, 1);
-            batch.draw(img, 400, 300, 100, 100);
-        }
-
-        batch.end();
     }
 
     @Override
@@ -101,7 +50,6 @@ public class FallingBlocks extends ApplicationAdapter {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+        assets.dispose();
     }
 }
