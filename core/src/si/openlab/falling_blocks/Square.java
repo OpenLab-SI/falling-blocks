752129268ef2a2fbf70bb7d88c0d33e13843fd38
Metod Medja <metod@medja.net>
2018-06-17 19:37:42 +0200

    Dodano fizikalno telo za kvadrat (Square)

--- core/src/si/openlab/falling_blocks/Square.java
+++ core/src/si/openlab/falling_blocks/Square.java
@@ -1,22 +1,57 @@
 package si.openlab.falling_blocks;
 
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 public class Square extends Actor {
-    Texture img;
+    // staticna polja za definicije oblike in telesa
+    // (staticna, ker so enaka za vse kvadrate)
+    static BodyDef bodyDef;
+    static FixtureDef fixtureDef;
 
-    Square(float x, float y, Color color, AssetManager assets) {
+    static {
+        // doloci obliko nasega kvadrata
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(0.5f, 0.5f);
+
+        // doloci fizikalne lastnosti kvadrata
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 500;
+
+        // doloci lastnoti telesa (kvadata) (znotraj fizikalnega pogona)
+        bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.fixedRotation = true;
+    }
+
+    Texture img;
+    Body body;
+    FallingBlocks game;
+
+    Square(float x, float y, Color color, FallingBlocks game) {
+        this.game = game;
+
         setSize(1, 1);
         setPosition(x, y);
         setColor(color);
 
-        img = assets.get("square.png");
+        img = game.assets.get("square.png");
+
+        // postavi kvadrat na pravi polozaj
+        bodyDef.position.set(getX() + 0.5f, getY() + 0.5f);
+        // ustvari telo
+        body = game.world.createBody(bodyDef);
+        // doda telesu kvadrat (sele tu to telo postane kvadrat)
+        body.createFixture(fixtureDef);
 
         addListener(new InputListener() {
             @Override
